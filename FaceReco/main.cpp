883fa6b4145d718699b74e7b47fc2fe09fbb9ad0
Yawo Kpeglo - Business Lab <ykpeglo@MacBauwens.local>
2014-02-24 16:04:06 +0100

    Initial Commit

---
+++ FaceReco/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  FaceReco
+//
+//  Created by Yawo Kpeglo - Business Lab on 24/02/2014.
+//  Copyright (c) 2014 Business Lab. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
